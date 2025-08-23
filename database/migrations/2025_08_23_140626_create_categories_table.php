5bec8db769c96d20d3514b5093412897a0ab1604
russel <rjcbuisan@usm.edu.ph>
2025-08-23 22:13:01 +0800

    Initial commit of new version

---
+++ database/migrations/2025_08_23_140626_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+
+             $table->foreign('parent_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+         Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign(['parent_id']);
+        });
+        
+        Schema::dropIfExists('categories');
+    }
+};
